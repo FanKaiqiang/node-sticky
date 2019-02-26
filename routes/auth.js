0d64bc991ccaa99e346d612c5b585f1a9b86ecaf
FanKaiqiang <fankaiqiang001@gmail.com>
2019-02-26 23:05:19 +0800

    路由

---
+++ routes/auth.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+
+
+
+passport.serializeUser(function(user, done) {
+  console.log('---serializeUser---')
+  console.log(user)
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  console.log('---deserializeUser---')
+  done(null, obj);
+});
+
+
+passport.use(new GitHubStrategy({
+    clientID: '7f330967357e5a6b2437',
+    clientSecret: '557596d43fa6ede04d554cb012e6987242f723f1',
+    callbackURL: "http://localhost:1234/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+    // });
+    done(null, profile);
+  }
+));
+
+
+router.get('/logout', function(req, res){
+  req.session.destroy();
+  res.redirect('/');
+})
+
+router.get('/github',
+  passport.authenticate('github'));
+
+router.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    req.session.user = {
+      id: req.user.id,
+      username: req.user.displayName || req.user.username,
+      avatar: req.user._json.avatar_url,
+      provider: req.user.provider
+    };
+    res.redirect('/');
+  });
+
+
+
+module.exports = router;
